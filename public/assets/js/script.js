e68516c826068d8d4aef3960da84a7328bf803a7
Ewunet Kassa <ewunetk@yahoo.com>
2019-05-14 16:16:03 -0400

    burger commit

---
+++ public/assets/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+        $(".devour-form").on("submit", function(event){
+            event.preventDefault();
+            console.log("submit devour")
+
+            var id = $(this).children(".burger_id").val().trim()
+            console.log("id: ", id)
+
+            $.ajax({
+                method: "PUT",
+                url: '/burgers/' + id  
+            }).then(function(data){
+                console.log("data: ", data)
+            
+            });
+
+        })
+
+
+
+
+});
