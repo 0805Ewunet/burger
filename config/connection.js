180ba6560a3f4970bfb2694788c9f15edb1fc4b4
Ewunet Kassa <ewunetk@yahoo.com>
2019-05-14 19:18:01 -0400

    commiting heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,38 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Etba@084113",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "todoagain_db",
+  });
+};
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+module.exports = connections;
+
+
+
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Etba@084113",
+//   database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+
 
 module.exports = connection;
